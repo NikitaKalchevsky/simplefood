3af8c979805b014617349e7fa3a96590b8161f89
Nikita.Kalchevsky <nikita.kalchevskyi@gmail.com>
2023-12-23 18:46:26 +0200

    23.12.2023

--- app/js/main.js
+++ app/js/main.js
@@ -25,6 +25,19 @@ new Swiper(".swiper", {
   },
 });
 
+new Swiper(".slider-reviews", {
+  direction: "horizontal",
+  speed: 700,
+
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   //Mobile Menu
   const burger = document.querySelector(".burger");
@@ -44,5 +57,78 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   const resizableSwiper = (
+//     breakpoint,
+//     swiperClass,
+//     swiperSettings,
+//     callback
+//   ) => {
+//     let swiper;
+
+//     breakpoint = window.matchMedia(breakpoint);
+
+//     const enableSwiper = function (className, settings) {
+//       swiper = new Swiper(className, settings);
+
+//       if (callback) {
+//         callback(swiper);
+//       }
+//     };
+
+//     const checker = function () {
+//       if (breakpoint.matches) {
+//         return enableSwiper(swiperClass, swiperSettings);
+//       } else {
+//         if (swiper !== undefined) swiper.destroy(true, true);
+//         return;
+//       }
+//     };
+
+//     breakpoint.addEventListener("change", checker);
+//     checker();
+//   };
+
+//   const someFunc = (instance) => {
+//     if (instance) {
+//       instance.on("slideChange", function (e) {
+//         console.log("*** Swiper.activeIndex", instance.activeIndex);
+//       });
+//     }
+//   };
+//   resizableSwiper("(max-width: 768px)", ".swiper3", {
+//     slidesPerView: 1,
+//     direction: "horizontal",
+//     speed: 700,
+//     pagination: {
+//       el: ".swiper-pagination-3",
+//       clickable: true,
+//     },
+//   });
+
+//   resizableSwiper("(max-width: 1200px)", ".swiper2", {
+//     direction: "horizontal",
+//     speed: 700,
+
+//     pagination: {
+//       el: ".swiper-pagination-2",
+//       clickable: true,
+//     },
+//   });
+
+//   resizableSwiper("(max-width: 3000px)", ".swiper", {
+//     direction: "horizontal",
+//     speed: 700,
+//     navigation: {
+//       nextEl: ".swiper-button-right",
+//       prevEl: ".swiper-button-left",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+// });
 
 var mixer = mixitup(".popular-category__list");
