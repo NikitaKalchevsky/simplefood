83c27b8afd34665d5ef331811032964392bbf3de
Nikita.Kalchevsky <nikita.kalchevskyi@gmail.com>
2023-12-24 18:51:02 +0200

    adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -7,37 +7,6 @@ window.addEventListener("scroll", function () {
     headerEl.classList.remove("header-menu");
   }
 });
-
-new Swiper(".swiper", {
-  direction: "horizontal",
-  speed: 700,
-  navigation: {
-    nextEl: ".swiper-button-right",
-    prevEl: ".swiper-button-left",
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
-
-new Swiper(".slider-reviews", {
-  direction: "horizontal",
-  speed: 700,
-
-  pagination: {
-    el: ".swiper-pagination-2",
-    clickable: true,
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   //Mobile Menu
   const burger = document.querySelector(".burger");
@@ -58,77 +27,121 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   const resizableSwiper = (
-//     breakpoint,
-//     swiperClass,
-//     swiperSettings,
-//     callback
-//   ) => {
-//     let swiper;
+// const swiperReviews = new Swiper(".reviews__slider", {
+//   direction: "horizontal",
+//   speed: 700,
+//   navigation: {
+//     nextEl: ".swiper-button-right",
+//     prevEl: ".swiper-button-left",
+//   },
 
-//     breakpoint = window.matchMedia(breakpoint);
-
-//     const enableSwiper = function (className, settings) {
-//       swiper = new Swiper(className, settings);
-
-//       if (callback) {
-//         callback(swiper);
-//       }
-//     };
-
-//     const checker = function () {
-//       if (breakpoint.matches) {
-//         return enableSwiper(swiperClass, swiperSettings);
-//       } else {
-//         if (swiper !== undefined) swiper.destroy(true, true);
-//         return;
-//       }
-//     };
-
-//     breakpoint.addEventListener("change", checker);
-//     checker();
-//   };
-
-//   const someFunc = (instance) => {
-//     if (instance) {
-//       instance.on("slideChange", function (e) {
-//         console.log("*** Swiper.activeIndex", instance.activeIndex);
-//       });
-//     }
-//   };
-//   resizableSwiper("(max-width: 768px)", ".swiper3", {
-//     slidesPerView: 1,
-//     direction: "horizontal",
-//     speed: 700,
-//     pagination: {
-//       el: ".swiper-pagination-3",
-//       clickable: true,
-//     },
-//   });
-
-//   resizableSwiper("(max-width: 1200px)", ".swiper2", {
-//     direction: "horizontal",
-//     speed: 700,
-
-//     pagination: {
-//       el: ".swiper-pagination-2",
-//       clickable: true,
-//     },
-//   });
-
-//   resizableSwiper("(max-width: 3000px)", ".swiper", {
-//     direction: "horizontal",
-//     speed: 700,
-//     navigation: {
-//       nextEl: ".swiper-button-right",
-//       prevEl: ".swiper-button-left",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   keyboard: {
+//     enabled: true,
+//   },
 // });
+
+// const restaurantsSwiper = new Swiper(".restaurants-swiper", {
+//   direction: "horizontal",
+//   slidesPerView: 1,
+//   speed: 400,
+//   pagination: {
+//     el: ".swiper-pagination-3",
+//     clickable: true,
+//   },
+//   keyboard: {
+//     enabled: true,
+//   },
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback
+  ) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    };
+
+    const destroySwiper = function () {
+      if (swiper !== undefined) {
+        swiper.destroy(true, true);
+        swiper = undefined;
+      }
+    };
+
+    const togglePaginationVisibility = function (visible) {
+      const paginationElement = document.querySelector(
+        swiperSettings.pagination.el
+      );
+      if (paginationElement) {
+        paginationElement.style.display = visible ? "block" : "none";
+      }
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        enableSwiper(swiperClass, swiperSettings);
+        togglePaginationVisibility(true);
+      } else {
+        destroySwiper();
+        togglePaginationVisibility(false);
+      }
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on("slideChange", function (e) {
+        console.log("*** Swiper.activeIndex", instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper("(max-width: 1200px)", ".restaurants-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    direction: "horizontal",
+    speed: 700,
+    pagination: {
+      el: ".swiper-pag",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  resizableSwiper("(max-width: 5000px)", ".reviews__slider", {
+    direction: "horizontal",
+    speed: 700,
+    navigation: {
+      nextEl: ".swiper-button-right",
+      prevEl: ".swiper-button-left",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
 
 var mixer = mixitup(".popular-category__list");
